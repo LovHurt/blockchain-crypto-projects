739cbd6c26295c5e61537b2ec5e8c653bc52f0fe
lovhurt <alicandagidir@hotmail.com>
2025-02-27 12:33:16 +0300

    add endpoints to retrieve blockchain and validate chain

--- can-chain/blockchain.py
+++ can-chain/blockchain.py
@@ -71,3 +71,20 @@ def mine_block():
                 'previous_hash': block['previous_hash']
                 }
     return jsonify(response), 200
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'The blockchain is valid.'}
+    else:
+        response = {'message': 'The blockchain is not valid.'}
+    return jsonify(response), 200
+
+app.run(host='0.0.0.0', port=5000)
