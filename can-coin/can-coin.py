2acf40f04881d6de58d07e65041a1db18eaa4f3d
lovhurt <alicandagidir@hotmail.com>
2025-02-28 13:44:28 +0300

    add node address and transaction creation in mine_block function

--- can-coin/can-coin.py
+++ can-coin/can-coin.py
@@ -89,6 +89,9 @@ class Blockchain:
 
 app = Flask(__name__)
 
+#creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-', '')
+
 blockchain = Blockchain()
 
 @app.route('/mine_block', methods = ['GET'])
@@ -97,12 +100,14 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_address, receiver = 'You', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']
+                'previous_hash': block['previous_hash'],
+                'transactions': block['transactions']
                 }
     return jsonify(response), 200
 
